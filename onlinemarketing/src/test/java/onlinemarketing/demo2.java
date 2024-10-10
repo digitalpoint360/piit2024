31ddf10eb870eab5c32b428cf8c4d53e458c8837
digitalpoint360 <74098030+digitalpoint360@users.noreply.github.com>
2024-10-09 21:36:53 -0400

    Update demo2.java

--- onlinemarketing/src/test/java/onlinemarketing/demo2.java
+++ onlinemarketing/src/test/java/onlinemarketing/demo2.java
@@ -5,7 +5,7 @@ public class demo2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println(" Hello IRM Team");
+		System.out.println("Hello IRM Team");
 
 	}
 
